7ced43867290fd719f8cf2771f82565cacba31b0
Ch3shireDev <the.smiling.cat.of.cheshire@gmail.com>
2020-10-27 23:26:46 +0100

    :rocket: Release to Electron

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require("electron");
+const url = require("url");
+const path = require("path");
+const server = require('./server.js');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  win.loadURL('http://localhost:3000');
+
+  // win.loadFile('./dist/index.html');
+  // win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
